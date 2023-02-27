3c3a5d170a5e05c1b0f4cd5abfd09c7d46672399
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-02-27 15:26:11 +0900

    infinite scroll

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import Menu from "../components/home/Menu";
 import Post from "../components/home/Post";
 import Sidebar from "../components/home/Sidebar";
 import { getPost, getTags, getPostByTag } from "../api";
 import { MdOutlineImageNotSupported } from "react-icons/md";
 import { useSearchParams } from "react-router-dom";
+import { throttle } from "throttle-debounce";
 
 import { PostType } from "../type";
 
@@ -12,23 +13,48 @@ const Home = () => {
   const [postData, setPostData] = useState<PostType[]>([]);
   const [isVisible, setIsVisible] = useState<boolean>(false);
   const [tags, setTags] = useState<string[]>([]);
+  const [page, setPage] = useState<number>(1);
+  const [srolled, setScrolled] = useState<number>(0);
+  const listRef = useRef<HTMLDivElement>(null);
+  const [isScrollBottom, setIsScrollBottom] = useState<boolean>(false);
   const [searchParams, setSearchParams] = useSearchParams();
-
+  const [loading, setLoading] = useState<boolean>(false);
   useEffect(() => {
     const tag = searchParams.get("tag");
     if (tag) {
       getPostByTag(tag).then((res) => {
         setPostData(res.data);
       });
+    } else if (page > 1) {
+      getPost(page).then((res) => {
+        setPostData((prev) => [...prev, ...res.data]);
+        setLoading(false);
+      });
     } else {
-      getPost().then((res) => {
+      getPost(page).then((res) => {
         setPostData(res.data);
       });
     }
     getTags().then((res) => {
       setTags(res.data);
     });
-  }, [searchParams]);
+  }, [searchParams, page]);
+
+  useEffect(() => {
+    if (isScrollBottom) {
+      setPage((prev) => prev + 1);
+      setLoading(true);
+    }
+  }, [isScrollBottom]);
+
+  const handleScroll = throttle(1000, () => {
+    if (listRef.current) {
+      const { scrollHeight, offsetHeight, scrollTop } = listRef.current;
+      setScrolled(scrollTop);
+      const offset = 50;
+      setIsScrollBottom(scrollHeight - offsetHeight - scrollTop < offset);
+    }
+  });
 
   return (
     <div className="flex justify-between lg:justify-center">
@@ -39,6 +65,8 @@ const Home = () => {
           className={`max-h-[calc(100vh-121px)] scrollbar-hide ${
             postData.length === 0 ? "overflow-hidden" : "overflow-y-auto"
           }`}
+          ref={listRef}
+          onScroll={handleScroll}
         >
           {postData.length === 0 ? (
             <div className="w-[calc(100vw-24px)] max-w-[660px] text-center h-[100vh]">
@@ -48,6 +76,7 @@ const Home = () => {
           ) : (
             postData.map((post) => <Post post={post} key={post._id} />)
           )}
+          {loading && <div className="text-lg text-center">loading</div>}
         </div>
       </div>
     </div>
