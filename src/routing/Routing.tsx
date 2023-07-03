d9ac351ea583cf45d800b6bd63037d05b6435b1f
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-07-03 11:39:09 +0900

    [refactor] 라우팅 파일 분리

---
+++ src/routing/Routing.tsx
@@ -0,0 +1,34 @@
+import React, { Suspense, lazy } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import ErrorBoundary from "../pages/Error";
+
+const Auth = lazy(() => import("../pages/Auth"));
+const Home = lazy(() => import("../pages/Home"));
+const PostDetail = lazy(() => import("../pages/PostDetail"));
+const Upload = lazy(() => import("../pages/Upload"));
+const UserDetail = lazy(() => import("../pages/UserDetail"));
+const Main = lazy(() => import("../components/common/Main"));
+
+const Routing = () => {
+  return (
+    // TODO fallback 컴포넌트 제작
+    <ErrorBoundary fallback={<p>에러 발생</p>}>
+      <Suspense fallback={<p>로딩 중...</p>}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Main />}>
+              <Route index element={<Home />} />
+              <Route path="user/:userId" element={<UserDetail />} />
+              <Route path="upload" element={<Upload />} />
+              <Route path="update/:postId" element={<Upload />} />
+            </Route>
+            <Route path="auth" element={<Auth />} />
+            <Route path="post/:postId" element={<PostDetail />} />
+          </Routes>
+        </BrowserRouter>
+      </Suspense>
+    </ErrorBoundary>
+  );
+};
+
+export default Routing;
