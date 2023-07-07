260f7f98e7b4587f35da96576b3a3b769e9d2557
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-07-07 15:45:11 +0900

    [refactor] useAuthUser 훅 위치 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import "swiper/css/bundle";
 import Routing from "./routing/Routing";
-import useAuthUser from "./hooks/useAuthUser";
 import { RecoilRoot } from "recoil";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
 function App() {
-  useAuthUser();
   return (
     <QueryClientProvider client={queryClient}>
       <RecoilRoot>
