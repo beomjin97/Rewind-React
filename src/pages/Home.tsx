e2b844ddcca52f801ae1f8dae5baf506770f39ba
[AI6/1반] 김범진 <qjawls646@gmail.com>
2023-07-11 14:55:40 +0900

    [refact] 홈 컴포넌트의 로직 Context로 추출

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import Menu from "../components/home/Menu";
-import Post from "../components/home/Post";
+import Post2 from "../components/home/Post2";
 import Sidebar from "../components/home/Sidebar";
 import { getPost, getTags, getPostByTag } from "../api";
 import { MdOutlineImageNotSupported } from "react-icons/md";
@@ -8,77 +8,36 @@ import { useSearchParams } from "react-router-dom";
 import { throttle } from "throttle-debounce";
 
 import { PostType } from "../types/post";
+import { usePost } from "../context/PostContext";
+import { Fragment } from "react";
+import Stack from "@mui/material/Stack";
 
 const Home = () => {
-  const [postData, setPostData] = useState<PostType[]>([]);
-  const [isVisible, setIsVisible] = useState<boolean>(false);
-  const [tags, setTags] = useState<string[]>([]);
-  const [page, setPage] = useState<number>(1);
-  const [srolled, setScrolled] = useState<number>(0);
-  const listRef = useRef<HTMLDivElement>(null);
-  const [isScrollBottom, setIsScrollBottom] = useState<boolean>(false);
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [loading, setLoading] = useState<boolean>(false);
-  useEffect(() => {
-    const tag = searchParams.get("tag");
-    if (tag) {
-      getPostByTag(tag).then((res) => {
-        setPostData(res.data);
-      });
-    } else if (page > 1) {
-      getPost(page).then((res) => {
-        setPostData((prev) => [...prev, ...res.data]);
-        setLoading(false);
-      });
-    } else {
-      getPost(page).then((res) => {
-        setPostData(res.data);
-      });
-    }
-    getTags().then((res) => {
-      setTags(res.data);
-    });
-  }, [searchParams, page]);
+  const PostConext = usePost();
+  if (PostConext === null) {
+    throw new Error("예상치 못한 에러가 발생했습니다.");
+  }
 
-  useEffect(() => {
-    if (isScrollBottom) {
-      setPage((prev) => prev + 1);
-      setLoading(true);
-    }
-  }, [isScrollBottom]);
-
-  const handleScroll = throttle(1000, () => {
-    if (listRef.current) {
-      const { scrollHeight, offsetHeight, scrollTop } = listRef.current;
-      setScrolled(scrollTop);
-      const offset = 50;
-      setIsScrollBottom(scrollHeight - offsetHeight - scrollTop < offset);
-    }
-  });
+  const { postList, error, fetchNextPage } = PostConext;
+  console.log(postList);
 
   return (
-    <div className="flex justify-between lg:justify-center">
-      <Sidebar isVisible={isVisible} tags={tags} />
-      <div>
-        <Menu setIsVisible={setIsVisible} />
-        <div
-          className={`max-h-[calc(100vh-121px)] scrollbar-hide ${
-            postData.length === 0 ? "overflow-y-hidden" : "overflow-y-auto"
-          }`}
-          ref={listRef}
-          onScroll={handleScroll}
-        >
-          {postData.length === 0 ? (
-            <div className="w-[calc(100vw-24px)] max-w-[660px] text-center h-[100vh]">
-              <MdOutlineImageNotSupported className="mx-auto mt-20 text-primary text-9xl" />
-              <p className="text-3xl font-thin">No Post not yet</p>
-            </div>
-          ) : (
-            postData.map((post) => <Post post={post} key={post._id} />)
-          )}
-          {loading && <div className="text-lg text-center">loading</div>}
-        </div>
+    <div className="grid grid-cols-3 gap-4 border-2">
+      <Stack position="sticky" className="border-2">
+        123
+      </Stack>
+      <div className="h-[100vh] scrollbar-hide overflow-y-auto">
+        <Stack spacing={6} className="items-center mt-2 border-2">
+          {postList?.pages.map((group, i) => (
+            <Fragment key={i}>
+              {group.map((post: any) => (
+                <Post2 />
+              ))}
+            </Fragment>
+          ))}
+        </Stack>
       </div>
+      <Stack className="border-2">123</Stack>
     </div>
   );
 };
